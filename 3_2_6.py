b1cc011fe2341de58d988c49704092591da68605
gbrs <61385511+gbrs@users.noreply.github.com>
2022-07-08 10:20:19 +0300

    solve 2.2.2

--- 3_2_6.py
+++ 3_2_6.py
@@ -1,13 +1,18 @@
 lst = []
 with open('1.csv') as f:
     for line in f:
-        lst.append(line.strip().replace(',', ' ').split())
+        lst.append(line.strip().split(','))
 
 # print(lst)
 
 col = lst[0].index('a')
 sm = 0
+cnt = 0
 for i in range(1, len(lst)):
-    sm += float(lst[i][col])
+    if lst[i][col]:
+        sm += float(lst[i][col])
+        cnt += 1
 
-print(round((sm + 0.0001) / (len(lst) - 1)))
+answer = round((sm + 0.0001) / cnt)
+
+print(answer)
